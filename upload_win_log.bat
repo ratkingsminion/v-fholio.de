f0365e37bccba5f69be560deba9b0b5c23e51d16
Friedrich Hanisch <friedrich@ratking.de>
2026-03-05 23:39:41 +0100

    use "time" instead of "checksum" now for upload / added new dev log entry

--- upload_win_log.bat
+++ upload_win_log.bat
@@ -2,6 +2,6 @@ REM remove "/log=upload.log" ?
 call upload_config.bat
 winscp.com /log=upload.log /command ^
     "open ftpes://%USER%:%PASS%@%HOST%" ^
-    "synchronize -criteria:checksum -transfer=auto -filemask=""| teaching/;things/;archive/"" remote ""%SOURCEFOLDER%\log\"" ""%TARGETFOLDER%\log\""" ^
+    "synchronize -criteria:time -transfer=auto -filemask=""| teaching/;things/;archive/"" remote ""%SOURCEFOLDER%\log\"" ""%TARGETFOLDER%\log\""" ^
     "close" ^
     "exit"
