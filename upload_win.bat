b8d01bc5b6185ef856d8e145f1b9c7daf97c734b
Friedrich Hanisch <friedrich@ratking.de>
2026-07-08 22:07:28 +0200

    change checksum to time for upload script

--- upload_win.bat
+++ upload_win.bat
@@ -2,6 +2,6 @@ REM remove "/log=upload.log" ?
 call upload_config.bat
 winscp.com /log=upload.log /command ^
     "open ftpes://%USER%:%PASS%@%HOST%" ^
-    "synchronize -criteria:checksum -transfer=auto -filemask=""| teaching/;things/;archive/"" remote ""%SOURCEFOLDER%"" ""%TARGETFOLDER%""" ^
+    "synchronize -criteria:time -transfer=auto -filemask=""| teaching/;things/;archive/"" remote ""%SOURCEFOLDER%"" ""%TARGETFOLDER%""" ^
     "close" ^
     "exit"
