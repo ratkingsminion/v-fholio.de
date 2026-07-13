fcdb4b8a1a79c1c5c33f3790f04618bb99be74e0
Friedrich Hanisch <friedrich@ratking.de>
2026-07-13 08:40:11 +0200

    update V to working server for live mode

--- live.sh
+++ live.sh
@@ -2,4 +2,5 @@
 trap 'rm v-fholio' INT
 trap 'rm v-fholio.exe' INT
 clear
-v -d veb_livereload watch --add data/content.json,data/projects.json --only-watch=*.html,*.css,*.js,*.md,*.tr,*.json,*.v,*.png,*.jp*g,*.gif run .
+v -g -d veb_livereload watch --add data/content.json,data/projects.json --only-watch=*.html,*.css,*.js,*.md,*.tr,*.json,*.v,*.png,*.jp*g,*.gif run .
+# v -g -prod -d veb_livereload watch --add data/content.json,data/projects.json --only-watch=*.html,*.css,*.js,*.md,*.tr,*.json,*.v,*.png,*.jp*g,*.gif run .
